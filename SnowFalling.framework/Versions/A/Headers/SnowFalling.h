8729037124bdc4eeca0aa4de7f8ee91b6c1dada2
Danilo Priore <danilo.priore@gmail.com>
2013-08-17 21:16:25 +0200

    optimized performance

--- SnowFalling.framework/Versions/A/Headers/SnowFalling.h
+++ SnowFalling.framework/Versions/A/Headers/SnowFalling.h
@@ -1,21 +1,41 @@
 //
 //  SnowFalling.h
-//  Snow Falling Effect iPhone SDK.
-//  Version: 1.0
+//  Snow Falling Effect iPhone/iPad SDK.
+//  Version: 1.4
 //
-//  Copyright 2011, Centro Studi Informatica di Danilo Priore. All rights reserved.
-//
+//  Copyright 2012, Centro Studi Informatica di Danilo Priore. All rights reserved.
+//  Internet: http://www.prioregroup.com
+//  Email support: support@prioregroup.com
 
-#import <Foundation/Foundation.h>
+#ifndef SNOWFALLING_H
+#define SNOWFALLING_H
+
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    SnowFlakeDirectionLeft = -1,
+    SnowFlakeDirectionVertical,
+    SnowFlakeDirectionRight,
+    SnowFlakeDirectionBoth
+} SnowFlakeDirections;
 
 @interface SnowFalling : NSObject
 
-
-@property (nonatomic, assign, setter = setHidden:) BOOL hidden;
+@property (nonatomic, assign) BOOL hidden;
+@property (nonatomic, assign) NSInteger numbersOfFlake;
+@property (nonatomic, assign) CGFloat minScaleSize;
+@property (nonatomic, assign) CGFloat maxScaleSize;
+@property (nonatomic, assign) SnowFlakeDirections directionsOfFlake;
+@property (nonatomic, retain) UIImage *imageOfFlake;
 
 - (id)initWithView:(UIView*)owner;
 - (id)initWithView:(UIView *)owner autoStartEnd:(BOOL)autoStartEnd;
 - (id)initWithView:(UIView *)owner startDate:(NSDate*)startDate endDate:(NSDate*)endDate;
 
+- (void)startAnimating;
+- (void)stopAnimating;
+
 @end
+
+#endif
